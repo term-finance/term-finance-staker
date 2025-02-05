6c372d58ace67f248e211990dcdbdfcae2ffd3a6
aazhou1 <andrew@termfinance.io>
2025-02-04 17:04:53 -0800

    m

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -6,24 +6,18 @@ pragma solidity 0.8.23;
 import {IERC20} from "openzeppelin/token/ERC20/IERC20.sol";
 import {Script} from "forge-std/Script.sol";
 
-import {DeployInput} from "script/DeployInput.sol";
 import {UniStaker} from "src/UniStaker.sol";
 import {V3FactoryOwner} from "src/V3FactoryOwner.sol";
 import {IERC20Delegates} from "src/interfaces/IERC20Delegates.sol";
 import {INotifiableRewardReceiver} from "src/interfaces/INotifiableRewardReceiver.sol";
 import {IUniswapV3FactoryOwnerActions} from "src/interfaces/IUniswapV3FactoryOwnerActions.sol";
 
-contract Deploy is Script, DeployInput {
-  uint256 deployerPrivateKey;
-
-  function setUp() public {
-    deployerPrivateKey = vm.envOr(
-      "DEPLOYER_PRIVATE_KEY",
-      uint256(0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80)
-    );
-  }
-
+contract Deploy is Script {
   function run() public returns (V3FactoryOwner, UniStaker) {
+    uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+    address STAKE_TOKEN_ADDRESS = vm.envAddress("STAKE_TOKEN_ADDRESS");
+    address PAYOUT_TOKEN_ADDRESS = vm.envAddress("PAYOUT_TOKEN_ADDRESS");
+
     vm.startBroadcast(deployerPrivateKey);
     // Deploy the staking contract
     UniStaker uniStaker = new UniStaker(
